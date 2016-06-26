6eafc4a71583b3ce8b55be505ca6ef41d1acb98d
Jeff Hoover <jhoover@jhoover.com>
2016-06-26 13:39:57 -0400

    More asserts - not failing when they should.

--- tests/check_hellomake.c
+++ tests/check_hellomake.c
@@ -2,12 +2,17 @@
 
 START_TEST (test_name)
 {
-  /* unit test code */
-ck_assert_int_eq(0,1);
+  int a=3, b=3;
+  ck_assert_int_eq(0,1);
+  ck_assert_msg(0==1,"Zero does not equal one.");
+  fail_unless(a+b==6, "err");
 }
 END_TEST
 
+
 int main(void)
 {
-return 0;
+   
+
+   return 0;
 }
