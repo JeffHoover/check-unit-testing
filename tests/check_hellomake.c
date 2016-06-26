9851daeeb57358bbe5865e093b54283cc94c02e0
Jeff Hoover <jhoover@jhoover.com>
2016-06-26 13:58:25 -0400

    changed assertion. Still doesn't fail.

--- tests/check_hellomake.c
+++ tests/check_hellomake.c
@@ -5,14 +5,12 @@ START_TEST (test_name)
   int a=3, b=3;
   ck_assert_int_eq(0,1);
   ck_assert_msg(0==1,"Zero does not equal one.");
-  fail_unless(a+b==6, "err");
+  fail_unless(a+b==16, "err");
 }
 END_TEST
 
 
 int main(void)
 {
-   
-
    return 0;
 }
